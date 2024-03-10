36dd1a2b48b418e494505179f28eca631f306795
Ethan Morris <nil.visits-0r@icloud.com>
2024-03-09 20:03:20 -0600

    update projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -7,6 +7,9 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  image: {
+    service: passthroughImageService()
+  },
   integrations: [mdx(), sitemap()],
   output: "server",
   adapter: cloudflare()
